b7a4167df54d1c06524152b937ff2994ba16e882
Pawnyy <sfaronoff@gmail.com>
2021-02-28 16:45:10 -0500

    added saving, and adding and removing manually

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions, MissingPermissions
+from discord import Member
 import time
+import json
 
 
 TOKEN = ''
@@ -8,12 +11,23 @@ PREFIX = '!'
 INTENTS = discord.Intents.default()
 bot = commands.Bot(command_prefix=PREFIX, intents=INTENTS)
 clockedIn = []
-clockInChannel = 806001968682237972
+printClockedIn = []
+clockInChannel = 707774381652181092
 
 @bot.event
 async def on_ready():
     print(f'Logged in as: {bot.user.name}')
     print(f'With ID: {bot.user.id}')
+    schedule.start()
+    autosave.start()
+    try:
+        with open("F:\downloads pt 2\discord\clockedIn.json", 'r') as listfile:
+            clockedIn = json.load(listfile)
+    except Exception:
+            clockedIn = []
+    return clockedIn
+
+
 
 
 
@@ -34,18 +48,39 @@ async def on_message(message):
         if message.content == "?clockout game":
             clockedIn.remove(message.author.id)
     print(clockedIn)
-    print(len(clockedIn))
- 
+    await bot.process_commands(message)
+
+@bot.command(pass_context=True)
+@has_permissions(manage_roles=True)
+async def add(ctx, member: Member):
+    await ctx.send("hi")
+    if member.id in clockedIn:
+        await ctx.send("That user is already clocked in.")
+    else:
+        clockedIn.append(member.id)
+
+@bot.command(pass_context=True)
+@has_permissions(manage_roles=True)
+async def remove(ctx, member: Member):
+    if member.id in clockedIn:
+        clockedIn.remove(member.id)
+    else:
+        await ctx.send("That user is not clocked in currently, sorry!")
     
 
 @tasks.loop(minutes=60)
 async def schedule():
     channel = bot.get_channel(clockInChannel)
-    print("test")
     for member in clockedIn:
         await channel.send(f"<@{member}> This is a reminder you are clocked in. Please clockout if you are not patrolling.")
 
-    
+@tasks.loop(seconds=30)
+async def autosave():
+    try:
+        with open("F:\downloads pt 2\discord\clockedIn.json", 'w') as listfile:
+            json.dump(clockedIn, listfile)
+    except Exception:
+        print("Oh, no! List wasn't saved! It'll be empty tomorrow...")
 
 
 bot.run(TOKEN)
